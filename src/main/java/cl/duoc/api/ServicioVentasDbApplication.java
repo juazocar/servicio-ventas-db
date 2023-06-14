0093cf71dcab1594384b128c26c16ecf2e214f3e
juazocar <juazocar@profesor.duoc.cl>
2023-06-13 21:15:15 -0400

    first commit

---
+++ src/main/java/cl/duoc/api/ServicioVentasDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioVentasDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioVentasDbApplication.class, args);
+	}
+
+}
